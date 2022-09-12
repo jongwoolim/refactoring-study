034991e3923c285bb406fdf0f6ca7ec7bbe2fc62
limjongwoo <whddn528@naver.com>
2022-09-12 12:24:21 +0900

    냄새 20 거대한 클래스(리팩토링 41: 슈퍼클래스 추출하기)

--- src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Department.java
+++ src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Department.java
@@ -2,29 +2,24 @@ package me.whiteship.refactoring._20_large_class._41_extract_superclass;
 
 import java.util.List;
 
-public class Department {
-
-    private String name;
+public class Department extends Party{
 
     private List<Employee> staff;
-
-    public String getName() {
-        return name;
-    }
 
     public List<Employee> getStaff() {
         return staff;
     }
 
-    public double totalMonthlyCost() {
-        return this.staff.stream().mapToDouble(e -> e.getMonthlyCost()).sum();
-    }
-
-    public double totalAnnualCost() {
-        return this.totalMonthlyCost() * 12;
+    public Department(String name) {
+        super(name);
     }
 
     public int headCount() {
         return this.staff.size();
+    }
+
+    @Override
+    public double monthlyCost() {
+        return this.staff.stream().mapToDouble(e -> e.monthlyCost()).sum();
     }
 }
